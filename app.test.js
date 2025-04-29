3a0b374a13ef26953197fe0fe51d75f5b87eadeb
Satyam Warghat <warghatsatyam33@gmail.com>
2025-04-29 16:06:02 +0000

    Made changes

--- app.test.js
+++ app.test.js
@@ -5,7 +5,7 @@ const request = require('supertest');
 const app = require('./app')
 
 describe('GET /', ()=>{
-    it(`should return Hello World`,async ()=>{
+    it(`should return`,async ()=>{
         const res = await request(app).get('/')
         expect(res.statusCode).toBe(200);    
         expect(res.text).toBe('Hello World');
