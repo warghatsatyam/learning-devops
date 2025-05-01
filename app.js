dca798e10badde3cb6256573105274b76655b95b
Satyam Warghat <warghatsatyam33@gmail.com>
2025-05-01 11:11:12 +0000

    Testing CD

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.send("Hello World from the other side");
 })
-
 
 module.exports = app;
