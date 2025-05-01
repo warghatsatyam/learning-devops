8693e0793df2eda8f10eb2c6fad540e8968117bd
Satyam Warghat <warghatsatyam33@gmail.com>
2025-05-01 11:16:14 +0000

    Modified the running port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const app = require('./app')
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080")
 })
